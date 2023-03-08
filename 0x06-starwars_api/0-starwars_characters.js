500039ee45f92a0bf3fb77b86850c7e0299b9daa
izimartin <izimartinz@gmail.com>
2023-03-08 01:32:30 +0100

    update

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,57 +1,22 @@
 #!/usr/bin/node
 
 const request = require("request");
+const filmId = process.argv[2];
+const url = `https://swapi-api.hbtn.io/api/films/${filmId}`;
 
-const movieId = process.argv[2];
-const filmEndPoint = "https://swapi-api.hbtn.io/api/films/" + movieId;
-let people = [];
-const names = [];
-
-const requestCharacters = async () => {
-  await new Promise((resolve) =>
-    request(filmEndPoint, (err, res, body) => {
-      if (err || res.statusCode !== 200) {
-        console.error("Error: ", err, "| StatusCode: ", res.statusCode);
-      } else {
-        const jsonBody = JSON.parse(body);
-        people = jsonBody.characters;
+request(url, async (err, response, body) => {
+  if (err) {
+    console.log(err);
+  }
+  for (const characterId of JSON.parse(body).characters) {
+    await new Promise((resolve, reject) => {
+      request(characterId, (err, response, body) => {
+        if (err) {
+          reject(err);
+        }
+        console.log(JSON.parse(body).name);
         resolve();
-      }
-    })
-  );
-};
-
-const requestNames = async () => {
-  if (people.length > 0) {
-    for (const p of people) {
-      await new Promise((resolve) =>
-        request(p, (err, res, body) => {
-          if (err || res.statusCode !== 200) {
-            console.error("Error: ", err, "| StatusCode: ", res.statusCode);
-          } else {
-            const jsonBody = JSON.parse(body);
-            names.push(jsonBody.name);
-            resolve();
-          }
-        })
-      );
-    }
-  } else {
-    console.error("Error: Got no Characters for some reason");
+      });
+    });
   }
-};
-
-const getCharNames = async () => {
-  await requestCharacters();
-  await requestNames();
-
-  for (const n of names) {
-    if (n === names[names.length - 1]) {
-      process.stdout.write(n);
-    } else {
-      process.stdout.write(n + "\n");
-    }
-  }
-};
-
-getCharNames();
+});
